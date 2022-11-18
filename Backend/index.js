c2b83b6d59f781a3b06a821c478a3394f969e583
Anshuman <anshuman@coffeebeans.io>
2022-11-18 13:00:34 +0530

    fetching all notes from API

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,20 @@
 const connectToMongo = require("./db");
-const express = require('express')
+var cors = require("cors");
+const express = require("express");
 
 connectToMongo();
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
-app.use(express.json())
+app.use(cors());
+
+app.use(express.json());
 
 // Available routes
 
-app.use('/api/authentication', require('./routes/authentication'))
-app.use('/api/notes', require('./routes/notes'))
+app.use("/api/authentication", require("./routes/authentication"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`iNotebook backend listening on port ${port}`)
-})
-
-
- 
+  console.log(`iNotebook backend listening on port ${port}`);
+});
